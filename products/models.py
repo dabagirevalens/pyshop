d93143b72215c90cf34fc1cf48d058e4985c2c37
dabagirevalens <83093092+dabagirevalens@users.noreply.github.com>
2021-10-09 08:55:43 +0200

    first commit

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.URLField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    discount = models.FloatField()
+
